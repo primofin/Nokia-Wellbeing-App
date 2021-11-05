9b6226713ad6a6fab58eb93333c0194d815850db
Vy Nguyen <vynguyen9699@gmail.com>
2021-11-05 13:18:18 +0200

    style closeForm modal

--- modals/CloseFormModal.tsx
+++ modals/CloseFormModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import {
   Heading,
   View,
@@ -11,18 +12,47 @@ import ThemeProvider from '../context/ThemeProvider'
 const CloseFormModal = ({ navigation }) => {
   return (
     <ThemeProvider>
-      <View>
-        <Heading>Are you sure you want to close the contact form?
+      <View style={styles.wrapper}>
+        <Heading size="md" style={styles.heading}>Are you sure you want to close the contact form?{'\n'}
           Your answers will not be saved</Heading>
-        <HStack space={3} alignItems="center">
-          <Button bg="primary.400" onPress={() => {
+        <HStack space={3} style={styles.buttonContainer}>
+          <Button style={styles.yesBtn} bg="primary.600" onPress={() => {
             navigation.navigate('Home')
           }}>Yes</Button>
-          <Button bg="danger.400" onPress={() => navigation.goBack()}>No</Button>
+          <Button style={styles.noBtn} bg="light.400" onPress={() => navigation.goBack()}>No</Button>
         </HStack>
       </View>
     </ThemeProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  heading: {
+    marginTop: 40,
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  yesBtn: {
+    width: 100,
+    borderRadius: 15,
+  },
+  noBtn: {
+    width: 100,
+    borderRadius: 15,
+  }
+})
 
 export default CloseFormModal
